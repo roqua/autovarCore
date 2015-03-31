22a66e8457549c79e0ed9e43f8e98508771b8a1e
Ando Emerencia <ando.emerencia@gmail.com>
2015-03-31 15:33:48 +0200

    added tests for needs_trend

--- R/needs_trend.r
+++ R/needs_trend.r
@@ -6,7 +6,8 @@
 #' @return A boolean indicating whether a trend is required for the specified VAR model.
 #' @examples
 #' data_matrix <- matrix(nrow = 40, ncol = 3)
-#' data_matrix[, ] <- runif(ncol(data_matrix) * nrow(data_matrix), 1, nrow(data_matrix))
+#' data_matrix[, ] <- runif(ncol(data_matrix) * nrow(data_matrix), 1, 10)
+#' data_matrix[, 3] <- (1:40) + rnorm(40)
 #' colnames(data_matrix) <- c('rumination', 'happiness', 'activity')
 #' data_matrix
 #' autovarCore:::needs_trend(data_matrix, 1)
