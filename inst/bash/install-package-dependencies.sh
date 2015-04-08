13fbaee4c0647346449392c654811859a22e1f1a
Ando Emerencia <ando.emerencia@gmail.com>
2015-04-08 19:22:50 +0200

    mirror install package dependencies script from grip-r

--- inst/bash/install-package-dependencies.sh
+++ inst/bash/install-package-dependencies.sh
@@ -1,1 +1,7 @@
-R --no-save --quiet -e 'options(repos=structure(c(CRAN="http://cran-mirror.cs.uu.nl/"))); install.packages(c("Amelia", "jsonlite", "urca", "vars", "testthat", "roxygen2", "Rcpp"));'
+#! /usr/bin/env Rscript
+options(repos = structure(c(CRAN = "http://cran-mirror.cs.uu.nl/")))
+packages <- c("Amelia", "jsonlite", "urca", "vars", "testthat", "roxygen2", "Rcpp")
+new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+if (length(new.packages))
+  install.packages(new.packages)
+update.packages(lib.loc = Sys.getenv("R_LIBS_USER"), ask = FALSE)
